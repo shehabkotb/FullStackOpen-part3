b5c6763cc8e11139c5634a284a7d5f4e92a7833f
Shehab Kotb <shehabktob@gmail.com>
2020-08-19 01:30:12 +0200

    A of part 3

--- index.js
+++ index.js
@@ -1,8 +1,101 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>")
+app.use(express.json())
+
+morgan.token("post-data", function getPostData(request) {
+  // debugger
+  return JSON.stringify(request.body)
+})
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :post-data"
+  )
+)
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>")
+})
+
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>phonebook has ${persons.length} people</p>
+  <p>${new Date()}</p>`
+  )
+})
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+  let toDisplay = persons.find(
+    (person) => person.id === Number(request.params.id)
+  )
+  if (!toDisplay) {
+    return response.status(404).end()
+  }
+  response.json(toDisplay)
+})
+
+app.post("/api/persons/", (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing"
+    })
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(409).json({
+      error: "name already exists"
+    })
+  }
+
+  let maxID = 99999999
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * Math.floor(maxID))
+  }
+  persons = persons.concat(newPerson)
+  response.status(201).end()
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+  let toDelete = persons.find(
+    (person) => person.id === Number(request.params.id)
+  )
+  if (!toDelete) {
+    return response.status(404).end()
+  }
+  persons = persons.filter((person) => person.id !== toDelete.id)
+  response.status(204).end()
 })
 
 const PORT = 3001
